5f79d959ce0135f919bec4573709f4ad69498c86
Samir Singh <ersamirsingh@gmail.com>
2026-01-12 15:32:21 +0530

    feat(remove and update): SELLER can remove and update product and its details

--- Server/src/Routers/ProductRouter.ts
+++ Server/src/Routers/ProductRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addProduct } from "../controllers/ProductController";
+import { addProduct, removeProduct, updateProduct } from "../controllers/ProductController";
 import authenticateSeller from "../middleware/authenticateSeller";
 
 
@@ -7,6 +7,8 @@ const productRouter: Router = Router();
 
 
 productRouter.post('/add', authenticateSeller, addProduct)
+productRouter.delete('/remove/:productId', authenticateSeller, removeProduct)
+productRouter.patch('/update/:productId', authenticateSeller, updateProduct);
 
 
 export default productRouter
--- Server/src/controllers/ProductController.ts
+++ Server/src/controllers/ProductController.ts
@@ -54,7 +54,22 @@ const removeProduct = async (req: Request, res: Response) => {
 
    try {
 
+      const productId = req.params.productId;
+      if(!productId)
+         return res.status(400).json({message: "Missing required fields"});
+
+      const product = await Product.findByIdAndDelete(productId);
+      if(!product)
+         return res.status(404).json({message: "Product not found"});
+
+      return res.status(200).json({message: "Product deleted successfully"});
+
    } catch (error) {
+
+      return res.status(500).json({
+         message:
+            error instanceof Error ? error.message : "Internal server error"
+      });
 
    }
 }
@@ -64,6 +79,49 @@ const removeProduct = async (req: Request, res: Response) => {
 
 const updateProduct = async (req: Request, res: Response) => {
 
+   try {
+            
+      const {productId} = req.params;
+      if(!productId)
+         return res.status(400).json({message: "Missing required fields"});
+
+      const product = await Product.findById(productId);
+      if(!product)
+         return res.status(404).json({message: "Product not found"});
+
+      const {
+         name,
+         description,
+         price,
+         discount,
+         stock,
+         images,
+      } = req.body || {};
+
+      if (name) product.name = name;
+      if (description) product.description = description;
+      if (price != null) product.price = price;
+      if (discount != null) product.discount = discount;
+      if (stock != null) product.stock = stock;
+      if (images) product.images.push(...images);
+
+      if (price != null || discount != null) {
+         const currentPrice = price ?? product.price;
+         const currentDiscount = discount ?? product.discount ?? 0;
+
+         product.finalPrice =
+         currentPrice - currentPrice * (currentDiscount / 100);
+      }
+
+      await product.save();
+
+      return res.status(200).json(product);
+
+   } catch (error) {
+      return res.status(500).json({
+         message: error instanceof Error ? error.message : "Internal server error"
+      });
+   }
 }
 
 
