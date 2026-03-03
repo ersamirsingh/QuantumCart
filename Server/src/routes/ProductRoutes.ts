2e3bb3f53398119ed4cf332eab6bf5cd399184dc
Samir Singh <ersamirsingh@gmail.com>
2026-03-03 23:04:47 +0530

    Fetch from prod

--- Server/src/routes/ProductRoutes.ts
+++ Server/src/routes/ProductRoutes.ts
@@ -8,7 +8,7 @@ import authenticateUser from '../middleware/authenticateUser';
 
 const productRouter: Router = Router();
 
-productRouter.get('/', authenticateUser, getAllProducts);
+productRouter.get('/', getAllProducts);
 productRouter.get('/:productId', authenticateUser, getProductById);
 
 export default productRouter;
